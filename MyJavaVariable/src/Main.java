93da734ade46e255d479139afb8d07aca7cd4bf8
Chun Hin D. Tang <dtang955472@gmail.com>
2021-11-08 17:44:08 -0500

    java practice variables

---
+++ MyJavaVariable/src/Main.java
@@ -0,0 +1,38 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+/*
+ * 		int x;			declaration
+ * 							+
+ * 		x = 123;		assignment
+ * 							=
+ * 		int x = 123;	initialization
+ */
+
+		int x = 123;
+//		System.out.println(x);
+		System.out.println("My number is: " + x);
+		
+//		long y = 12345678987654321L;
+//		System.out.println(y);
+//		System.out.println("My number is: " + y);
+		
+//		float y = 3.14f;
+		double y = 3.14;
+		System.out.println(y);
+		
+		boolean z = true;
+		System.out.println(z);
+		
+		char symbol = '@';
+		System.out.println(symbol);
+		
+		String name = "Chun";
+		System.out.println(name);
+		System.out.println("Hello " + name);
+		
+	}
+
+}
