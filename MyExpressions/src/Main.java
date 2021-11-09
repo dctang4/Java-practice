ce250a3b07bc94de2f786934bada0b6375404dcc
Chun Hin D. Tang <dtang955472@gmail.com>
2021-11-08 19:31:33 -0500

    java expressions

--- MyExpressions/src/Main.java
+++ MyExpressions/src/Main.java
@@ -7,9 +7,13 @@ public class Main {
 		// operands = values, variables, numbers, quantity
 		// operators = + - * / %
 		
-		int friends = 10;
+//		int friends = 10;
+		double friends = 10;
 		
-		friends--;
+//		friends++;
+//		friends--;
+		
+//		friends = friends / 3; // for int division the decimal values is truncated
 		
 		System.out.println(friends);
 
