d2a0ee265222cd345f323ac11fd2e42ae629d0fe
Chun Hin D. Tang <dtang955472@gmail.com>
2021-11-11 14:56:12 -0500

    java final keyword

---
+++ 25-MyFinalKeyword/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// final = anything declared with this keyword cannot be changed/updated later in the program
+		// it's common practice to declare a final variable in all caps
+		
+		final double PI = 3.14159;
+		
+		System.out.println(PI);
+
+	}
+
+}
