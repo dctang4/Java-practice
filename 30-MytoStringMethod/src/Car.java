387a877aec21e6a8e7dbf9726ee8f8d19a3eacdd
Chun Hin D. Tang <dtang955472@gmail.com>
2021-11-15 17:30:48 -0500

    java toString method

---
+++ 30-MytoStringMethod/src/Car.java
@@ -0,0 +1,21 @@
+
+public class Car {
+
+	String make = "Ford";
+	String model = "Mustang";
+	String color = "red";
+	int year = 2021;
+	
+	
+	// method override (override the default method with your own)
+	public String toString() {
+		
+//		String myString = make +"\n"+ model +"\n"+ color +"\n"+ year;
+//		return myString;
+		
+		return make +"\n"+ model +"\n"+ color +"\n"+ year;
+		
+	}
+	
+	
+}
---
+++ 30-MytoStringMethod/src/Main.java
@@ -0,0 +1,28 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// toString() =	special method that all objects inherit,
+		//				that returns a string that "textually represents" an object.
+		//				can be used both implicitly and explicitly
+		
+		Car car = new Car();
+		
+//		System.out.println(car.make);
+//		System.out.println(car.model);
+//		System.out.println(car.color);
+//		System.out.println(car.year);
+		
+		// using toString() explicitly
+//		System.out.println(car.toString());
+		
+		// or
+		
+		// using toString() implicitly
+		System.out.println(car);
+		
+
+	}
+
+}
