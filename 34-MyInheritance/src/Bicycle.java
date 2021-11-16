245bf900eb2e38779c71cc979b048c81fb9090c3
Chun Hin D. Tang <dtang955472@gmail.com>
2021-11-15 19:04:03 -0500

    java inheritance

---
+++ 34-MyInheritance/src/Bicycle.java
@@ -0,0 +1,9 @@
+
+public class Bicycle extends Vehicle {
+
+	// Vehicle is the parent class and Bicycle is the child class
+	
+	int wheels = 2;
+	int pedals = 2;
+	
+}
---
+++ 34-MyInheritance/src/Car.java
@@ -0,0 +1,9 @@
+
+public class Car extends Vehicle {
+
+	// Vehicle is the parent class and Car is the child class
+	
+	int wheels = 4;
+	int doors = 4;
+	
+}
