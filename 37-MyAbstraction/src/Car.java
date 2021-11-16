f3e6f405ecf1f1dcebf3eadac13e008a79a7b18a
Chun Hin D. Tang <dtang955472@gmail.com>
2021-11-16 00:09:17 -0500

    java abstraction

---
+++ 37-MyAbstraction/src/Car.java
@@ -0,0 +1,15 @@
+
+public class Car extends Vehicle {
+
+	// Car subclass must implement the abstract method go() from parent class
+	
+	@Override
+	void go() {
+		
+		System.out.println("The driver is driving the car");
+		
+	}
+
+	
+	
+}
