de884833d395b7531554f5e22818b5ee7b9ee939
Chun Hin D. Tang <dtang955472@gmail.com>
2021-11-16 17:05:56 -0500

    java dynamic polymorphism

---
+++ 43-MyDynamicPolymorphism/src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// polymorphism = many shapes/forms
+		// dynamic = after compilation (during runtime)
+		
+		// ex: A corvette is a: corvette, and a car, and a vehicle, and an object
+		
+		// Dynamic Polymorphism = ability to take many shapes/forms after the program starts running
+		
+		Scanner scanner = new Scanner(System.in);
+		Animal animal;
+		
+		System.out.println("What animal do you want?");
+		System.out.print("(1=dog) or (2=cat): ");
+		int choice = scanner.nextInt();
+		
+		if(choice == 1) {
+			animal = new Dog();
+			animal.speak();
+		}
+		else if(choice == 2) {
+			animal = new Cat();
+			animal.speak();
+		}
+		else {
+			animal = new Animal();
+			System.out.println("That choice was invalid");
+			animal.speak();
+		}
+
+	}
+
+}
