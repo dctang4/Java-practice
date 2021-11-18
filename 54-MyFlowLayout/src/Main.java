1d38b4ce8688978ab32dc79429045e3a5f0af07e
Chun Hin D. Tang <dtang955472@gmail.com>
2021-11-18 01:41:00 -0500

    java GridLayout

--- 54-MyFlowLayout/src/Main.java
+++ 54-MyFlowLayout/src/Main.java
@@ -37,6 +37,16 @@ public class Main {
 		panel.add(new JButton("8"));
 		panel.add(new JButton("9"));
 		
+//		frame.add(new JButton("1"));
+//		frame.add(new JButton("2"));
+//		frame.add(new JButton("3"));
+//		frame.add(new JButton("4"));
+//		frame.add(new JButton("5"));
+//		frame.add(new JButton("6"));
+//		frame.add(new JButton("7"));
+//		frame.add(new JButton("8"));
+//		frame.add(new JButton("9"));
+		
 		frame.add(panel);
 		frame.setVisible(true);
 	}
