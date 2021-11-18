c78e6368d75252b2904628014d113c4c18283467
Chun Hin D. Tang <dtang955472@gmail.com>
2021-11-18 00:40:23 -0500

    java panels edit

--- 51-MyPanels/src/Main.java
+++ 51-MyPanels/src/Main.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.BorderLayout;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -16,23 +17,23 @@ public class Main {
 		JLabel label = new JLabel();
 		label.setText("Hi");
 		label.setIcon(image);
-//		label.setVerticalAlignment(JLabel.TOP);
-//		label.setHorizontalAlignment(JLabel.LEFT);
+		label.setVerticalAlignment(JLabel.CENTER);
+		label.setHorizontalAlignment(JLabel.CENTER);
 
 		JPanel redPanel = new JPanel();
 		redPanel.setBackground(Color.red);
 		redPanel.setBounds(0, 0, 250, 250);
-//		redPanel.setLayout(new BorderLayout());
+		redPanel.setLayout(new BorderLayout());
 		
 		JPanel bluePanel = new JPanel();
 		bluePanel.setBackground(Color.blue);
 		bluePanel.setBounds(250, 0, 250, 250);
-//		bluePanel.setLayout(new BorderLayout());
+		bluePanel.setLayout(new BorderLayout());
 		
 		JPanel greenPanel = new JPanel();
 		greenPanel.setBackground(Color.green);
 		greenPanel.setBounds(0, 250, 500, 250);
-//		greenPanel.setLayout(new BorderLayout());
+		greenPanel.setLayout(new BorderLayout());
 		
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
