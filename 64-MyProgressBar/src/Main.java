67cc3adba39d79c53ec12e7ab16e89d7ae924bb1
Chun Hin D. Tang <dtang955472@gmail.com>
2021-11-23 15:53:13 -0500

    java progress bar

---
+++ 64-MyProgressBar/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// JProgressBar = Visual aid to let the user know that an operation is processing
+		
+		new ProgressBarDemo();
+		
+	}
+
+}
