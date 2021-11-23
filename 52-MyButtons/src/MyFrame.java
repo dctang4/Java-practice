86814fb56b6fc11c5db91ecd3a4ea21abe448720
Chun Hin D. Tang <dtang955472@gmail.com>
2021-11-22 21:56:29 -0500

    java ComboBox

--- 52-MyButtons/src/MyFrame.java
+++ 52-MyButtons/src/MyFrame.java
@@ -1,5 +1,5 @@
-//import java.awt.event.ActionEvent;
-//import java.awt.event.ActionListener;
+// import java.awt.event.ActionEvent;
+// import java.awt.event.ActionListener;
 
 import java.awt.Font;
 import java.awt.Color;
@@ -10,7 +10,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-//public class MyFrame extends JFrame implements ActionListener {
+// public class MyFrame extends JFrame implements ActionListener {
 public class MyFrame extends JFrame {
 	
 	JButton button;
---
+++ 62-MyComboBox/src/MyFrame.java
@@ -0,0 +1,33 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class MyFrame extends JFrame {
+
+	
+	MyFrame() {
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLayout(new FlowLayout());
+		
+		String[] animals = {"dog", "cat", "bird"};
+		JComboBox comboBox = new JComboBox(animals);
+		
+		comboBox.addActionListener(e -> {
+			System.out.println(comboBox.getSelectedItem());
+			System.out.println(comboBox.getSelectedIndex());
+		});
+		
+		comboBox.setEditable(true);  // allows you to type and edit the text-box
+//		System.out.println(comboBox.getItemCount());
+//		comboBox.addItem("horse");  // adds an item to the end of the comboBox
+//		comboBox.insertItemAt("pig", 0);  // adds an item at index 0 but will not be selected
+//		comboBox.setSelectedIndex(0);  // sets the selected item
+//		comboBox.removeItem("cat");  // removes an item by name
+//		comboBox.removeItemAt(0);  // removes an item by index
+//		comboBox.removeAllItems();  // removes all items
+
+		
+		this.add(comboBox);
+		this.pack();
+		this.setVisible(true);
+	}
+}
