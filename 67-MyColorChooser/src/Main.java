410522472a28bbd66640ac25cf252e0e185f71fe
Chun Hin D. Tang <dtang955472@gmail.com>
2021-11-25 16:20:19 -0500

    java color chooser

--- 67-MyColorChooser/src/Main.java
+++ 67-MyColorChooser/src/Main.java
@@ -3,6 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		// JColorChooser = A GUI mechanism that let's a user choose a color
+		
 		new MyFrame();
 
 	}
--- 67-MyColorChooser/src/MyFrame.java
+++ 67-MyColorChooser/src/MyFrame.java
@@ -3,7 +3,34 @@ import javax.swing.*;
 
 public class MyFrame extends JFrame {
 
+	JButton button;
+	JLabel label;
+	
 	MyFrame() {
+		
+		button = new JButton("Pick a Color");
+		button.addActionListener(e -> {
+			JColorChooser colorChooser = new JColorChooser();
+			
+			Color color = JColorChooser.showDialog(null, "Pick a color", Color.black);
+			
+			label.setForeground(color);
+//			label.setBackground(color);
+		});
+		
+		label = new JLabel();
+		label.setBackground(Color.white);
+		label.setText("This is some text.");
+		label.setFont(new Font("MV Boli", Font.PLAIN, 100));
+		label.setOpaque(true);
+		
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLayout(new FlowLayout());
+		this.add(button);
+		this.add(label);
+		this.pack();
+		this.setVisible(true);
 		
 	}
 	
